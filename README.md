18c2cf2f7f43d809c2714c575596968755d461d3
Mark Yaghi <markyaghi@hotmail.com>
2022-05-10 14:18:40 -0600

    VIN error checking fully functional on 'recall.html', see README.md for specifics.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=7789677&assignment_repo_type=AssignmentRepo)
+This program is a proof-of-concept for a site designed to assist the user in researching and gathering the necessary information to purchase a vehicle or find out information about any vehicle. The site consists of 5 pages: index.html, reviews.html, recalls.html, search.html, and about.html. Each page has its own dedicated JavaScript page, titled the same as its corresponding html page. That is, recalls.js is for the recalls.html page, about.js is for the about.html page, and so on. 
+
+Because this site is desgined to be a proof-of-concept, it does not contain all of the functionality that it would had it been a full-on deployable website open to the public. All the error checking on all the pages is functional, however, so all that's needed is to hook up the site to a proper back end/ server set up and it'll be a fully functioning web site.
+
+IMPORTANT  NOTES: 
+
+1) The recall.html page is currently connected to the pokemon API. It is functional, and returns 6 data items from the API. Therefore, to return data, the names of Pokemon characters must be entered (i.e., pikachu, ditto, scyther,etc.).
+2) To enable the (fully functional) VIN error checking on the recall.html page, the do as follows: 
+    a) Remove the "/*" from line 27 and place it in front of " */ " on line 32. 
+    b) Move the " // " in front of "vinSearchTerm" on line 37 and place in front of "vinSearchTerm" on line 38.
+    c) Add a " /* " in front of the word "fetch" on line 40.
+    d) Add a " */ " on line 90.
+    e) Add a " // " in front of "pictureDiv..." on line 101.
+    f) Remove the " /* " from in front of "function....." on line 107 and place it in front of the " */ " on line 139.
+
+3) To enble the API and disable the VIN error checking, do number 2 above in reverse.
--- src/js/recalls.js
+++ src/js/recalls.js
@@ -1,10 +1,11 @@
+
 let btnRecallSubmit = document.querySelector("#btnRecallSubmit");
 //let btnReviewSubmit = document.querySelector("#btnReviewSubmit");
 let btnClear = document.querySelector("#btnClear");
 let vinSearchBar = document.querySelector("#vinSearchBar");
 let vinSearchResults = document.querySelector("#vinSearchResults");
 let pictureDiv=document.querySelector("#pictureDiv");
-
+let vinSearchTerm;
 
 
 //btnReviewSubmit.addEventListener("click");
@@ -12,80 +13,85 @@ let pictureDiv=document.querySelector("#pictureDiv");
 btnRecallSubmit.addEventListener("click",e=> 
 {
 
-e.preventDefault();
-let vinSearchTerm = vinSearchBar.value;
-vinSearchTerm= vinSearchTerm.trim();
+        e.preventDefault();
+        vinSearchTerm = vinSearchBar.value;
+        vinSearchTerm= vinSearchTerm.trim();
 
-vinSearchResults.innerText="";
+        vinSearchResults.innerText="";
 
-if(vinSearchTerm.length==0)
-{
-    alert("Please enter a 17 character VIN to search.");
-    clrButton();
-}
-
-else
-{
-    clrButton();  //clear the screen to make room for new search results
-    
-   
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${vinSearchTerm}`) // Specify your API endpoint here.
-        .then(response =>
+        if(vinSearchTerm.length==0)
+        {
+            alert("Please enter a 17 character VIN to search.");
+            clrButton();
+        }
+       /* else if(validateVin(vinSearchTerm)!=true)    
          {
-            if (response.ok)
-            {
-                return response.json();
-            }
-            else
-            {
-                vinSearchResults.innerText = "Sorry, I couldn't find the name/character you asked for. Please try again.";
-                return false;
-            }
-        })
+            
+            alert("The VIN you entered:" +vinSearchTerm+ " is not a valid VIN. Please enter a valid VIN." );
+            clrButton();
+         }   */
 
-        .then(data =>
-        { // In these braces, you have your JSON data. Create object with relevant properties and assign the JSON data to them
-
-            if (data)
-            {                       
-                let characterDescription = 
+        else
+        {
+            clrButton();  //clear the screen to make room for new search results   
+           // vinSearchTerm=vinSearchTerm.toUpperCase();  // <<---Enable this line for VIN error checking; disable to use Pokemon API.
+            vinSearchTerm=vinSearchTerm.toLowerCase();  // <<---Enable this line for the Pokemon API, disable to use VIN error checking. 
+           
+         fetch(`https://pokeapi.co/api/v2/pokemon/${vinSearchTerm}`) // Specify your API endpoint here.
+               .then(response =>
                 {
-                    speciesName: data.species.name,
-                    type1: data.types[0].type.name,
-                    type2: data.types.length > 1 ? data.types[1].type.name : "",   //ternary or inline if statement
-                    weight: data.weight,
-                    height: data.height,
-                    baseExperience: data.base_experience,
-                    spritesPic:data.sprites.other.dream_world.front_default,          //get the relevant character's picture
-                }
+                  if (response.ok)
+                    {
+                      return response.json();
+                    }
+                  else
+                    {
+                      vinSearchResults.innerText = "Sorry, I couldn't find the name/character you asked for. Please try again.";
+                      return false;
+                    }
+                })
 
-                var img = document.createElement("img");        //create an image variable;
-                img.src=(characterDescription.spritesPic);      //give it the source/address;
-                var src=document.getElementById("pictureDiv");    //create an element to point to the div
-                src.appendChild(img);                              //put the pic in there
+                .then(data =>
+                { // In these braces, you have your JSON data. Create object with relevant properties and assign the JSON data to them
+
+                    if (data)
+                    {                       
+                      let characterDescription = 
+                        {
+                          speciesName: data.species.name,
+                          type1: data.types[0].type.name,
+                          type2: data.types.length > 1 ? data.types[1].type.name : "",   //ternary or inline if statement
+                          weight: data.weight,
+                          height: data.height,
+                          baseExperience: data.base_experience,
+                          spritesPic:data.sprites.other.dream_world.front_default,          //get the relevant character's picture
+                        }
+
+                        var img = document.createElement("img");        //create an image variable;
+                        img.src=(characterDescription.spritesPic);      //give it the source/address;
+                        var src=document.getElementById("pictureDiv");    //create an element to point to the div
+                        src.appendChild(img);                              //put the pic in there
 
 
+                        if (characterDescription.type2.length == 0)   //The purpose of this is simply for better output display. If type two is empty, we omit its position and the comma ahead of it.
+                        {
+                           vinSearchResults.innerText = `Here are the characteristics of ${characterDescription.speciesName.toUpperCase()} : \nDescription: ${characterDescription.type1}; \nWeight: ${characterDescription.weight}; \nHeight: ${characterDescription.height}; \nBase Experience: ${characterDescription.baseExperience}.`;
+                        }
+                        else  //otherwise, display type2 in the proper format.
+                        {
+                           vinSearchResults.innerText = `Here are the characteristics of ${characterDescription.speciesName.toUpperCase()}:  \nDescriptions: ${characterDescription.type1}, ${characterDescription.type2}; \nWeight: ${characterDescription.weight}; \nHeight: ${characterDescription.height}; \nBase Experience: ${characterDescription.baseExperience}.`;
+                        }
+                    }                                  
 
-                if (characterDescription.type2.length == 0)   //the purpose of this is simply for better output display. If type two is empty, we omit its position and the comma ahead of it.
-                {
-                    vinSearchResults.innerText = `Here are the characteristics of ${characterDescription.speciesName.toUpperCase()} : \nDescription: ${characterDescription.type1}; \nWeight: ${characterDescription.weight}; \nHeight: ${characterDescription.height}; \nBase Experience: ${characterDescription.baseExperience}.`;
-                }
-                else  //otherwise, display type2 in the proper format.
-                {
-                    vinSearchResults.innerText = `Here are the characteristics of ${characterDescription.speciesName.toUpperCase()}:  \nDescriptions: ${characterDescription.type1}, ${characterDescription.type2}; \nWeight: ${characterDescription.weight}; \nHeight: ${characterDescription.height}; \nBase Experience: ${characterDescription.baseExperience}.`;
-                }
-            }                                  
-
-        })
-        .catch(exception => {vinSearchResults.innerText = exception; })// Promise style catch, to be used after .then() statements.   
-                         
-       .finally(() => { vinSearchResults.innerText += " \n\nSearch Request complete."}, (console.log(" \n\nSearch Request complete.")));// Promise style finally, to be used after .then() and .catch() statements, will run regardless of if the catch fires.                        
-         
-
-}
-
+                })
+                .catch(exception => {vinSearchResults.innerText = exception; })// Promise style catch, to be used after .then() statements.   
+                                    
+                .finally(() => { vinSearchResults.innerText += " \n\nSearch Request complete."}, (console.log(" \n\nSearch Request complete.")));// Promise style finally, to be used after .then() and .catch() statements, will run regardless of if the catch fires.                        
+              /*  */  
+        }
 });
+
+btnClear.addEventListener("click", clrButton());
 
 function clrButton()
 {
@@ -97,32 +103,38 @@ function clrButton()
 
  // <--------------------------------------------BELOW IS THE CODE TO VERIFY THE VIN ENTERED ------------------------------------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
- /*   //let vin=" ";                      // Real VIN for testing from a Ford Explorer Sport:   1FMCU9DZ8MUB12974       1fmcu9dz8mub12974 ;         fake 17 char vin: 0fmcu9di8muo12974
-    let validVin=false;  
-              
-        while (validVin==false)         
-       {
-                                        
-        // vin= await input("Please enter a 17 character VIN for your vehicle consisting of ONLY letters and numbers, EXCEPT: NO i, I, o,O, q, Q: ");
-        // vin=vin.trim();
+
+ /* function validateVin (vinSearchTerm)
+  {
+                     // Real VIN for testing from a Ford Explorer Sport:   1FMCU9DZ8MUB12974       1fmcu9dz8mub12974 ;         fake 17 char vin: 0fmcu9di8muo12974
+    
+         let vinBool=false;
+         vinSearchTerm = vinSearchBar.value;
+         vinSearchTerm=vinSearchTerm.trim();
+        
          
           if(vinSearchTerm.length==17 && checkVin(vinSearchTerm)==true)  // check to see if the client entered the correct number of characters. If so, call the checkVin function.
           {    
             vinSearchTerm=vinSearchTerm.toUpperCase();
-             output("The VIN you entered: "+vinSearchTerm+" is a valid VIN." );
-             validVin=true;     
+             alert("The VIN you entered:" +vinSearchTerm+ " is a valid VIN." );
+             vinBool=true;
+               
           }
              
           else if (vinSearchTerm.length!=17)        // if they've entered anything other than exactly 17 characters, let them know, and set flag to false to make them try again.
           {                       
-            alert("The VIN you entered does not have 17 characters. It has "+vinSearchTerm.length+" characters. Or you may have entered characters other than letters and numbers. Please enter a VIN with exactly 17 characters.");
+            alert("The VIN you entered: "+vinSearchTerm.toUpperCase()+" does not have 17 characters. It has "+vinSearchTerm.length+" characters. Or you may have entered characters other than letters and numbers. Please enter a VIN with exactly 17 characters.");
            
-             validVin=false;   
+             clrButton();
+             vinBool=false;
           }                          
          
-       }   // end of while loop
-
-       function checkVin(vinSearchTerm)
-       {
-          return  /^[A-HJ-NPR-Za-hj-npr-z0-9]*$/.test(vinSearchTerm);   //check to make sure the user entered ONLY letters (upper and lower case) and numbers. No 0 (zero) i, I, o, O or other characters acceptable.                   
-       }*/
+         return vinBool;
+  }
+    
+function checkVin(vinSearchTerm)
+  {
+    
+   return  /^[A-HJ-NPR-Za-hj-npr-z0-9]{17},*$/.test(vinSearchTerm);   //check to make sure the user entered ONLY letters (upper and lower case) and numbers. No 0 (zero) i, I, o, O or other characters acceptable.                   
+  }*/
+    
